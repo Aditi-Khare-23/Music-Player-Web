bbe411445d3f66985ad016ddc603adaf3b752cfa
Aditi-Khare-23 <81019914+Aditi-Khare-23@users.noreply.github.com>
2023-03-18 01:51:17 +0530

    client
    client

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,10 +7,24 @@ import { useStateValue } from "../context/StateProvider";
 import { getAuth } from "firebase/auth";
 import { app } from "../config/firebase.config";
 
+import { motion } from "framer-motion";
 const Header = () => {
   const navigate = useNavigate();
   const [{ user }, dispatch] = useStateValue();
-  console.log(user);
+
+  const [isMenu, setIsMenu] = useState(false);
+
+  const logout = () => {
+    const firebaseAuth = getAuth(app);
+    firebaseAuth
+      .signOut()
+      .then(() => {
+        window.localStorage.setItem("auth", "false");
+      })
+      .catch((e) => console.log(e));
+    navigate("/login", { replace: true });
+  };
+  //console.log(user);
   return (
     <header className="flex items-center w-full p-4 md:py-2 md:px-6">
       <NavLink to={"/"}>
@@ -37,13 +51,23 @@ const Header = () => {
         </li>
       </ul>
 
-      <div className="flex items-center ml-auto cursor-pointer gap-2 relative">
-        <img
+      <div
+        onMouseEnter={() => setIsMenu(true)}
+        onMouseLeave={() => setIsMenu(false)}
+        className="flex items-center ml-auto cursor-pointer gap-2 relative"
+      >
+        {/* <img
           src={user?.user?.imageURL}
           className="w-12 h-12 min-w-[44px] object-cover rounded-full shahdow-lg"
           alt=""
+        /> */}
+        <img
+          className="w-12 min-w-[44px] object-cover rounded-full shadow-lg"
+          src={user?.user?.imageURL}
+          alt=""
+          referrerpolicy="no-referrer"
         />
-        <div className="flex flex-col">
+        {/* <div className="flex flex-col">
           <p className="text-textColor text-lg hover:text-headingColor font-semibold">
             U{user ? user.name : ""}
           </p>
@@ -51,7 +75,42 @@ const Header = () => {
             Premium Member.{" "}
             <FaCrown className="text-sm -ml-1 text-yellow-500" />{" "}
           </p>
+        </div> */}
+        <div className="flex flex-col">
+          <p className="text-textColor text-lg hover:text-headingColor font-semibold">
+            i{user?.user.name}
+          </p>
+          <p className="flex items-center gap-2 text-xs text-gray-500 font-normal">
+            Premium Member.{" "}
+            <FaCrown className="text-xm -ml-1 text-yellow-500" />{" "}
+          </p>
         </div>
+
+        {isMenu && (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            exit={{ opacity: 0, y: 50 }}
+            className="absolute z-10 top-12 p-3 right-0 w-275 gap-2 bg-card shadow-lg rounded-lg backdrop-blur-sm flex flex-col"
+          >
+            <NavLink to={"/userProfile"}>
+              <p className="text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out">
+                Profile
+              </p>
+            </NavLink>
+            <p className="text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out">
+              My Favourites
+            </p>
+            <hr />
+
+            <p
+              className="text-base text-textColor hover:font-semibold duration-150 transition-all ease-in-out"
+              onClick={logout}
+            >
+              Sign Out
+            </p>
+          </motion.div>
+        )}
       </div>
     </header>
   );
